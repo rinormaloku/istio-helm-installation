97f9226f3f9f71167d81b86fbc1d7f9785890b19
rinormaloku <rinormaloku37@gmail.com>
2022-01-29 22:20:42 +0100

    upd

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 kind create cluster --name istio-argo 
 
 kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
+kubectl apply -n argocdn -f argocd-cm.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -l 'app.kubernetes.io/name != argocd-cm' 
 kubectl -n argocd wait po --for condition=Ready --timeout -1s --all   
 
 echo "Use the password shown below. Username is 'admin'"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d | copy
+
+## Needed for HPA
+kubectl apply -f metrics-server.yaml
 
 kubectl port-forward svc/argocd-server -n argocd 8080:443 
 
